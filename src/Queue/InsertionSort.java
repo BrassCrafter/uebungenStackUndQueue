b6d3007815d20b078af37aa606338e374792fcef
BrassCrafter <tillmann-rohlfing@protonmail.com>
2023-11-30 22:19:47 +0100

    Übungsaufgaben zum Stack und zur Queue
    Took 16 minutes

--- src/Queue/InsertionSort.java
+++ src/Queue/InsertionSort.java
@@ -2,10 +2,10 @@ package Queue;
 public class InsertionSort {
     public static void sort(Queue<Integer> queue){
         if(!queue.isEmpty()){
-            int meins = queue.front();
+            int temp = queue.front();
             queue.dequeue();
             sort(queue);
-            insert(queue, meins);
+            insert(queue, temp);
             reverse(queue);
         }
     }
@@ -13,23 +13,23 @@ public class InsertionSort {
         if(queue.isEmpty()){
             queue.enqueue(neu);
         }else{
-            int meins = queue.front();
+            int temp = queue.front();
             queue.dequeue();
-            if(meins > neu){
-                insert(queue, meins);
+            if(temp > neu){
+                insert(queue, temp);
                 queue.enqueue(neu);
             }else{
                 insert(queue, neu);
-                queue.enqueue(meins);
+                queue.enqueue(temp);
             }
         }
     }
     private static void reverse(Queue<Integer> queue){
         if(!queue.isEmpty()){
-            int meins = queue.front();
+            int temp = queue.front();
             queue.dequeue();
             reverse(queue);
-            queue.enqueue(meins);
+            queue.enqueue(temp);
         }
     }
 }
